3dc4e8f8d13fd60c7c7f360da9d24d46d5f0fa6b
Rafael Santos <rafa.05125@gmail.com>
2024-05-18 03:14:12 -0300

    Models

---
+++ ToDoApp/Models/Tarefa.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using ToDoApp.Enums;
+
+namespace ToDoApp.Models
+{
+    public class Tarefa
+    {
+        public Tarefa()
+        {
+            this.DataCriacao = DateTime.Now;
+            this.DataAtualizacao = DateTime.Now;
+        }
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+        public int UsuarioId { get; set; }
+        public Status Status { get; set; }
+    }
+}
