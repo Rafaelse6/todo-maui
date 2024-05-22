197b653033534a057a7ca6eefc19c82e02fbb146
Rafael Santos <rafa.05125@gmail.com>
2024-05-21 23:03:31 -0300

    Cleaning main page

--- ToDoApp/MainPage.xaml.cs
+++ ToDoApp/MainPage.xaml.cs
@@ -6,7 +6,6 @@ namespace ToDoApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         DatabaseServico<Tarefa> _tarefaService;
 
@@ -21,20 +20,7 @@ namespace ToDoApp
         private async void CarregarTarefas()
         {
             var tarefas = await _tarefaService.TodosAsync();
-            TarefasCollectionView.ItemsSource = tarefas;
             TarefasCollectionTable.ItemsSource = tarefas;
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Cliquei aqui {count} vezes";
-            else
-                CounterBtn.Text = $"Cliquei aqui {count} vezes";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 
