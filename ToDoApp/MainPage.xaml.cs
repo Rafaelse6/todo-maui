857a1da9f30172c74c00a4c35970ac677ca96ac8
Rafael Santos <rafa.05125@gmail.com>
2024-05-28 22:30:14 -0300

    Creating table with action button and binding

--- ToDoApp/MainPage.xaml.cs
+++ ToDoApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ToDoApp.Constants;
+﻿using System.Windows.Input;
+using ToDoApp.Constants;
 using ToDoApp.Models;
 using ToDoApp.Services;
 
@@ -9,12 +10,22 @@ namespace ToDoApp
 
         DatabaseServico<Tarefa> _tarefaService;
 
+        public ICommand AlertCommand { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
             _tarefaService = new DatabaseServico<Tarefa>(Db.DB_PATH);
 
+            AlertCommand = new Command<Tarefa>(ExecuteAlertCommand);
+            TarefasCollectionTable.BindingContext = this;
+
             CarregarTarefas();
+        }
+
+        private void ExecuteAlertCommand(Tarefa tarefa)
+        {
+            DisplayAlert("Alerta", $"Tarefa: {tarefa.Titulo}", "OK");
         }
 
         private async void CarregarTarefas()
